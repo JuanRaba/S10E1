3e994bbe4daf363b4b7193078117996f853738d1
juanraba <jrabanillog@gmail.com>
2018-08-23 16:46:43 -0300

    ejer3

--- ejer3.rb
+++ ejer3.rb
@@ -9,3 +9,27 @@ class Vehicle
     @start = true
   end
 end
+
+class Car < Vehicle
+  @@num_instancias = 0
+  def initialize(model, year)
+    super(model, year)
+    @@num_instancias+=1
+  end
+
+  def engine_start
+    super
+    puts "el motor se ha encendido!"
+  end
+
+  def self.num_instancias
+    @@num_instancias
+  end
+end
+
+c = Car.new("model", 1980)
+c.engine_start
+
+10.times { Car.new("model", 1980) } # tricky way as no destructor found
+
+puts Car.num_instancias
