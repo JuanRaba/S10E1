ed6f12d3468cca2d7f30fc6ea249690080baec7f
juanraba <jrabanillog@gmail.com>
2018-08-23 18:00:14 -0300

    ejer6needFix

---
+++ ejer6.rb
@@ -0,0 +1,37 @@
+module Test
+  def result
+    if (@nota1+@nota2)>8
+      "Estudiante aprobado"
+    else
+      "Estudiante reprobado"
+    end
+  end
+end
+
+module Attendance
+  def student_quantity
+    666#@@quantity
+  end
+end
+
+class Student
+  @@quantity = 0
+  def initialize(nombre, nota1=4, nota2=4)
+    @nombre = nombre
+    @nota1 = nota1
+    @nota2 = nota2
+    @@quantity += 1
+  end
+
+  include Test
+  extend Attendance
+end
+
+students = []
+for i in 1..10 do
+  s = Student.new( "student#{i}", rand(1..10), rand(1..10) )
+  puts s.result
+  students.push s
+end
+
+puts Student.student_quantity
